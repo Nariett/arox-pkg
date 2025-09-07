20d927eb871232a91a932153f5dab7de3f896a28
Sasha <samsiki50@gmail.com>
2025-09-08 00:57:29 +0300

    update migrate

--- db/migrate.go
+++ db/migrate.go
@@ -7,16 +7,41 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+func findMigrationsPath() string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 5; i++ {
+		testPath := filepath.Join(currentDir, "schema", "migrations")
+		if _, err := os.Stat(testPath); err == nil {
+			path := "file://" + testPath
+
+			path = strings.Replace(path, "\\", "/", -1)
+			return path
+		}
+		currentDir = filepath.Dir(currentDir)
+	}
+
+	log.Fatal("Migrations directory not found")
+	return ""
+}
+
 func Migrate(db *sqlx.DB) {
+	migrationsPath := findMigrationsPath()
 
 	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	m, err := migrate.NewWithDatabaseInstance("file://schema//migrations", "postgres", driver)
+	m, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,6 +49,4 @@ func Migrate(db *sqlx.DB) {
 	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		log.Fatal(err)
 	}
-	log.Println("Migrations successfully applied!")
-
 }
